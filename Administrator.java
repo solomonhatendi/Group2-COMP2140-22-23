f04b8731ca0bb15c5b464c9e54d3e412e1be24c6
Solomon <81330585+solomonhatendi@users.noreply.github.com>
2022-12-04 07:23:05 -0500

    Made changes to Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,116 +1,212 @@
 package g;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
+/**
+ *
+ * @author group 2
+ *
+ */
+ 
+public class LoginPage extends javax.swing.JDialog {
 
-// Login screen
-public class Administrator extends JFrame{
-    private JLabel usernameLabel;
-    private JTextField usernameField;
-    private JLabel passwordLabel;
-    private JPasswordField passwordField;
-    private JFrame adminFrame;
-    public Administrator(){
-        // Setting configuration for Administrator window
-        super("Dawn's Grocery Inventory Management System - Admin");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //setPreferredSize(getToolkit().getScreenSize());
-        int length = 200;
-        int width = 400;
-        setPreferredSize(new Dimension(width,length));
-        setResizable(false); //window not resizable
-
-        // Creating panel for admin components
-        JPanel adminPnl = new JPanel();
-
-        // Adding username and password fields
-        usernameLabel = new JLabel("Username:");
-        usernameLabel.setFont(new Font("Arial", Font.PLAIN, 13));
-        usernameField = new JTextField(20);
-
-        passwordLabel = new JLabel("Password:");
-        passwordLabel.setFont(new Font("Arial", Font.PLAIN, 13));
-        passwordField = new JPasswordField(20);
-
-     
-        JButton adminLoginBtn = new JButton("Login");
-        adminLoginBtn.setBounds(300, 430, 200, 68);
-        adminLoginBtn.addActionListener(new AdminLoginBtnListener());
-
-        getContentPane().add(adminPnl, BorderLayout.EAST);
-        
-        JButton closeButton = new JButton("Close");
-        closeButton.addActionListener(new closeBtnListener());
-        GroupLayout gl_adminPnl = new GroupLayout(adminPnl);
-        gl_adminPnl.setHorizontalGroup(
-        	gl_adminPnl.createParallelGroup(Alignment.LEADING)
-        		.addGroup(gl_adminPnl.createSequentialGroup()
-        			.addGap(22)
-        			.addGroup(gl_adminPnl.createParallelGroup(Alignment.LEADING)
-        				.addComponent(adminLoginBtn)
-        				.addComponent(usernameLabel)
-        				.addComponent(passwordLabel))
-        			.addGap(31)
-        			.addGroup(gl_adminPnl.createParallelGroup(Alignment.TRAILING)
-        				.addGroup(gl_adminPnl.createSequentialGroup()
-        					.addPreferredGap(ComponentPlacement.RELATED)
-        					.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        					.addContainerGap())
-        				.addGroup(gl_adminPnl.createSequentialGroup()
-        					.addPreferredGap(ComponentPlacement.RELATED)
-        					.addComponent(usernameField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        					.addContainerGap())
-        				.addGroup(gl_adminPnl.createSequentialGroup()
-        					.addComponent(closeButton)
-        					.addContainerGap())))
-        );
-        gl_adminPnl.setVerticalGroup(
-        	gl_adminPnl.createParallelGroup(Alignment.LEADING)
-        		.addGroup(gl_adminPnl.createSequentialGroup()
-        			.addGap(26)
-        			.addGroup(gl_adminPnl.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(usernameLabel)
-        				.addComponent(usernameField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-        			.addGap(18)
-        			.addGroup(gl_adminPnl.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(passwordLabel)
-        				.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-        			.addGap(35)
-        			.addGroup(gl_adminPnl.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(adminLoginBtn)
-        				.addComponent(closeButton))
-        			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        adminPnl.setLayout(gl_adminPnl);
+    /**
+     * Creates new form LoginPage
+     */
+    public LoginPage(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
     }
 
-    // When the user clicks the login button the username and password will be checked
-    private class AdminLoginBtnListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // When the user successfully logs in, they will have access to the menu options
-            if(usernameField.getText().equals("admin") && (String.valueOf(passwordField.getPassword()).equals("password"))){
-                Menu menuFrame = new Menu();
-                menuFrame.pack();
-                menuFrame.setVisible(true);
-                dispose();
-                //setVisible(false);
-            } else {
-                JOptionPane.showMessageDialog(null, "Incorrect Username or Password.");
+    @SuppressWarnings("unchecked")
+                              
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        loginHeader = new javax.swing.JLabel();
+        usernameField = new javax.swing.JTextField();
+        usernameLabel = new javax.swing.JLabel();
+        passwordField = new javax.swing.JPasswordField();
+        passwordLabel = new javax.swing.JLabel();
+        loginButton = new javax.swing.JButton();
+        loginCancelButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Dawn's Grocery Store Inventory Management System");
+        setBackground(new java.awt.Color(204, 204, 204));
+        setMinimumSize(new java.awt.Dimension(358, 232));
+        setName("Dialogin"); // NOI18N
+        setPreferredSize(new java.awt.Dimension(358, 251));
+        setResizable(false);
+        setSize(new java.awt.Dimension(358, 251));
+
+        loginHeader.setFont(new java.awt.Font("FreeSans", 1, 18)); // NOI18N
+        loginHeader.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        loginHeader.setText("Dawn's Grocery");
+
+        usernameField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                usernameFieldActionPerformed(evt);
+            }
+        });
+
+        usernameLabel.setText("username:");
+
+        passwordField.setText("password");
+        passwordField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordFieldActionPerformed(evt);
+            }
+        });
+
+        passwordLabel.setText("password:");
+
+        loginButton.setText("Login");
+        loginButton.setMaximumSize(new java.awt.Dimension(76, 24));
+        loginButton.setMinimumSize(new java.awt.Dimension(76, 24));
+        loginButton.setPreferredSize(new java.awt.Dimension(76, 24));
+        loginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginButtonActionPerformed(evt);
+            }
+        });
+
+        loginCancelButton.setText("Cancel");
+        loginCancelButton.setName(""); // NOI18N
+        loginCancelButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginCancelButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(78, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(passwordLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(usernameLabel))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(usernameField, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(52, 52, 52)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(loginCancelButton, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(loginButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(loginHeader, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(82, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(39, Short.MAX_VALUE)
+                .addComponent(loginHeader, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(usernameLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(usernameField))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(passwordLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(loginButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(loginCancelButton)
+                .addGap(23, 23, 23))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }                                                                   
+
+    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        if (usernameField.getText().equals("admin") && (String.valueOf(passwordField.getPassword()).equals("password"))) {
+            MainPage menuFrame = new MainPage();
+            menuFrame.pack();
+            menuFrame.setVisible(true);
+            dispose();
+        } else {
+                javax.swing.JOptionPane.showMessageDialog(null, "Incorrect username or password.");
                 usernameField.setText("");
                 passwordField.setText("");
             }
+    }                                           
 
+    private void loginCancelButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+        System.exit(0);
+    }                                                 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                LoginPage dialog = new LoginPage(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
     }
-    
-    private class closeBtnListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-        	System.exit(0);
-        }
-    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton loginButton;
+    private javax.swing.JButton loginCancelButton;
+    private javax.swing.JLabel loginHeader;
+    private javax.swing.JPasswordField passwordField;
+    private javax.swing.JLabel passwordLabel;
+    private javax.swing.JTextField usernameField;
+    private javax.swing.JLabel usernameLabel;
+    // End of variables declaration                   
 }
